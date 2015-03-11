4d09fd04c4def07920dd9c4e4b62278cb98aa873
NikolaiPerepelitza <nikolaip17@riverdale.k12.or.us>
2015-03-11 10:48:38 -0700

    Create fade.ino

---
+++ fade.ino
@@ -0,0 +1,13 @@
+int led = 9;           
+int brightness = 0;    
+int fadeAmount = 5;    
+void setup()  { 
+  pinMode(led, OUTPUT);
+} 
+void loop()  {
+  analogWrite(led, brightness);    
+  if (brightness == 0 || brightness == 255) {
+    fadeAmount = -fadeAmount ; 
+  }     
+  delay(30);                            
+}
